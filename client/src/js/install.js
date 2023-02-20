34e79b29316e7d8b8c01ea3b1a4e3e0fffca96ba
magmesser <magmesser@gmail.com>
2023-02-20 18:31:36 -0500

    debugging install program functionality

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,27 +3,16 @@ const butInstall = document.getElementById("buttonInstall");
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
-  window.deferredPrompt = event;
-
-  butInstall.classList.toggle("hidden", false);
+  event.preventDefault();
+    butInstall.style.visibility = 'visible';
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
-  const promptEvent = window.deferredPrompt;
-
-  if (!promptEvent) {
-    return;
-  }
-
-  promptEvent.prompt();
-
-  window.deferredPrompt = null;
-
-  butInstall.classList.toggle("hidden", true);
+  butInstall.setAttribute('disabled', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
-  window.deferredPrompt = null;
+  console.log('😇', 'appinstalled', event);
 });
